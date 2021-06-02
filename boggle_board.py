9a8ee1e58a437f5b16fb5b9f862c56628a5816f6
Kate Kim <katehrkim@gmail.com>
2021-06-02 17:29:23 -0400

    Kate's answer

--- boggle_board.py
+++ boggle_board.py
@@ -1,6 +1,19 @@
+import random
+import string
 class BoggleBoard:
   
   def __init__(self):
-  
-
+    self.board = [['_','_','_','_'],['_','_','_','_'],['_','_','_','_'],['_','_','_','_']]
+    
   def shake(self):
+    for i, list in enumerate(self.board):
+      for j, elem in enumerate(list):
+        random_letter = random.choice(string.ascii_uppercase)
+        if random_letter == 'Q':
+          random_letter = 'Qu'
+        self.board[i][j] = random_letter
+
+test = BoggleBoard()
+print(test.board)
+test.shake()
+print(test.board)
